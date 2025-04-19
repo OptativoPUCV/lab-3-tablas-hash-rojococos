f5e17ce0d69842398fbdd4b9db4d86202e9db509
Your Name <you@example.com>
2025-04-19 18:41:05 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,59 +41,52 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
     if (map->size == map->capacity){
-        printf("la tabla hash esta llena");
+        printf("la tabla hash esta llena\n");
         return;
     }
 
     if (searchMap(map, key) != NULL){
-        printf("la clave ya existe");
+        printf("la clave ya existe\n");
         return;
     }
+
     
     size_t posi = hash(key, map->capacity);
-    while (map->buckets[posi] != NULL){
-        posi++;
+    while (1){
+        if(map->buckets[posi] == NULL || map->buckets[posi]->key == NULL){
+            Pair * nuevoPar = createPair(key, value);
+            map->buckets[posi] = nuevoPar;
+            map->size++;
+            map->current = posi;
+            return;
+            }
+        else posi = (posi+1) % map->capacity;
     }
-
-    
-    if (posi == map->capacity){
-        posi = 0; //por si el bucket libre estaba más atras
-        while (map->buckets[posi] != NULL){
-            posi++;
-        }
-    }
-
-    Pair * nuevoPar = createPair(key, value);
-    map->buckets[posi] = nuevoPar;
-    map->size++;
-    map->current = posi;
-    return;
 }
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
-    HashMap * nuevoMap = createMap(map->capacity * 2);
-    nuevoMap->capacity = map->capacity * 2;
-    nuevoMap->size = 1;
-    map->size --;
-    Pair * Elemento = firstMap(map);
-    insertMap(nuevoMap, Elemento->key, Elemento->value);
-    while(map->size != 0){
-        nuevoMap->size ++;
-        Pair * Elemento = nextMap(map);
-        insertMap(nuevoMap, Elemento->key, Elemento->value);
-        map->size --;
-        
+    long cap_antigua = map->capacity;
+    map->capacity = map->capacity * 2;
+    
+    Pair ** buckets_antiguos = map->buckets;
+    map->buckets = (Pair **)calloc(map->capacity, sizeof(Pair *));
+    map->size = 0;
+
+    
+    for(size_t l = 0; l < cap_antigua; l++){
+        if(buckets_antiguos[l] != NULL && buckets_antiguos[l]->key != NULL){
+            insertMap(map, buckets_antiguos[l]->key, buckets_antiguos[l]->value);
+        }
     }
-    free(map);
-    map = nuevoMap;
+    free(buckets_antiguos);
     return;
 }
 
 
 HashMap * createMap(long capacity) {
     HashMap * Mapa = (HashMap *)malloc(sizeof(HashMap));
-    Mapa->buckets = (Pair **)malloc(sizeof(Pair *) * capacity); //esto deberia dejar nulas todas las casillas bucket
+    Mapa->buckets = (Pair **) calloc(capacity, sizeof(Pair *)); //esto deberia dejar nulas todas las casillas bucket
     Mapa->size = 0;
     Mapa->capacity = capacity;
     Mapa->current = -1;
@@ -102,49 +95,73 @@ HashMap * createMap(long capacity) {
 }
 
 
-//no menciona que se considere la opcion de que la clave no este
 void eraseMap(HashMap * map,  char * key) {    
     size_t posi = hash(key, map->capacity);
-    while(map->buckets[posi]->key != key){
-        if(posi == map->capacity) posi = 0;
-        else posi++;
+    if(map->buckets[posi] == NULL) return;
+    while(map->buckets[posi] != NULL){
+        if(is_equal(map->buckets[posi]->key, key)){
+            map->buckets[posi]->key = NULL;
+            map->size--;
+            return;
+        }
+        posi = (posi+1) % map->capacity;
     }
-    map->buckets[posi]->key = NULL;
-    map->size --;
-    //no menciona el actualizar el current
-
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
     size_t posiTeo = hash(key, map->capacity);
+    if(map->buckets[posiTeo] == NULL) {
+        //printf("la clave no esta\n");
+        return NULL;
+    }
     while(map->buckets[posiTeo] != NULL){
         if(is_equal(map->buckets[posiTeo]->key, key)){
             map->current = posiTeo;
             return map->buckets[posiTeo];
         }
-        if(posiTeo == map->capacity) posiTeo = 0;
-        else posiTeo++;
+        posiTeo = (posiTeo+1) % map->capacity;
     }
-    printf("la clave no esta");
+    //printf("la clave no esta\n");
     return NULL;
 }
 
 Pair * firstMap(HashMap * map) {
-    size_t posi = 0;
-    while(map->buckets[posi] == NULL || map->buckets[posi]->key == NULL){
-        posi++;
+    for (size_t posi = 0; posi < map->capacity; posi++) {
+        if (map->buckets[posi] != NULL && map->buckets[posi]->key != NULL) {
+            map->current = posi;
+            return map->buckets[posi];
+        }
     }
-
-    map->current = posi;
-    return map->buckets[posi];
+    map->current = -1;
+    return NULL;
 }
+
 
 Pair * nextMap(HashMap * map) {
-    size_t posi = map->current + 1;
-    while(map->buckets[posi] == NULL || map->buckets[posi]->key == NULL){
-        if(posi == map->capacity) posi = 0;
-        posi++;
+    if(map->current == -1) return NULL;
+    //long aux = map->current + 1;
+    /* while(aux != map->current - 1){
+        if(aux >= map->capacity) aux = 0;
+        if(map->buckets[aux] != NULL)
+            if(map->buckets[aux]->key != NULL){
+                map->current = aux;
+                return map->buckets[aux];
+            }
+        aux++;
+        
     }
-    map->current = posi;
-    return map->buckets[posi];
+    printf("no hay otro elemento valido aparte del primero\n");
+    return NULL;
+    */
+    for (long aux = map->current + 1; aux < map->capacity; aux++){
+        if(map->buckets[aux] != NULL)
+            if(map->buckets[aux]->key != NULL){
+                map->current = aux;
+                return map->buckets[aux];
+            }
+        
+    }
+    return NULL;
 }
+
+
